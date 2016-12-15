715f4aa4d6486816bfe2f2f97fb3110da1d78e2b
Wenqiang Bo <bowenqiang@live.com>
2016-12-15 01:09:41 -0600

    minor fix

--- admin/config/connection.php
+++ admin/config/connection.php
@@ -1,4 +1,4 @@
--<?php
- -# Database Connection Here...
- -$dbc = mysqli_connect('127.0.0.1', 'root', '701717', 'MySite') OR die('Could not connect because: '.mysqli_connect_error());
- -?>
+<?php
+ # Database Connection Here...
+ $dbc = mysqli_connect('127.0.0.1', 'root', '701717', 'MySite') OR die('Could not connect because: '.mysqli_connect_error());
+ ?>
